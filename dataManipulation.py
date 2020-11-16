f9d3bf53f9944706a998a08695a78ccdf8baeb60
grantw99 <grantw99@gmail.com>
2020-11-16 17:57:05 -0500

    Adding in resized images

--- dataManipulation.py
+++ dataManipulation.py
@@ -2,6 +2,8 @@ import os
 import cv2
 import numpy as np
 import random
+import math
+import imutils
 
 def randResize(cv2_in, new_path):
     blank_image = np.zeros((45, 45, 3), np.uint8)
@@ -21,6 +23,26 @@ def randResize(cv2_in, new_path):
     print("Write new image to: {}".format(new_path))
     cv2.imwrite("./new.jpg", blank_image)
 
+def randRotate(cv2_in, new_path):
+    degs = random.randint(-15,15)
+    print(degs)
+    new_angle = math.radians(degs)
+    new_size = abs(int((45*45)//(math.sin(new_angle)*45-math.cos(new_angle)*45)))
+    cv2.resize(cv2_in, (new_size, new_size))
+    rotated = imutils.rotate_bound(cv2_in, new_angle)
+
+    blank_image = np.zeros((45, 45, 3), np.uint8)
+    blank_image[:,0:45] = (255, 255, 255)
+
+    blank_image[:,:] = rotated
+
+    cv2.imshow("image", blank_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+randRotate(cv2.imread('./all_imgs/train/A/17_a_137.jpg'), './new.jpg')
+"""
 all_dir = './all_imgs'
 for tt in os.listdir(all_dir):
     for class_folder in os.listdir("{}/{}".format(all_dir, tt)):
@@ -29,3 +51,4 @@ for tt in os.listdir(all_dir):
             resized_path = "{}/{}/{}/{}".format(all_dir, tt, class_folder, "_resized.".join(img.split('.')))
             im_in = cv2.imread(im_path)
             randResize(im_in, resized_path)
+"""
